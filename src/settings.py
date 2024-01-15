c88e8b053ce79c8b9cb79c1234cb220e2c49576f
peixed351 <peixed351@gmail.com>
2024-01-15 18:39:27 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "SYNTHIA-PANO"
 PROJECT_NAME_FULL: str = "SYNTHIA-PANO Panoramic Image Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
